be9d1004b55e8a7c0f0eba213c237f0126412b54
mateus040 <mateusgabrielmoreno264@gmail.com>
2023-06-21 14:16:47 -0300

    criando interface inicial

--- Curso JavaScript-Completo/instagram-clone/src/App.js
+++ Curso JavaScript-Completo/instagram-clone/src/App.js
@@ -1,18 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
-import {db} from'./firebase.js';
-import { useEffect } from 'react';
+import { db } from './firebase.js';
+import { useEffect, useState } from 'react';
 
 function App() {
 
-  useEffect(()=>{
-    console.log(db);
-  },[])
+  // Para saber qual tela mostrar para o usuário
+  const [user, setUser] = useState('Mateus');
+
+  useEffect(() => {
+
+
+
+  }, [])
 
 
   return (
     <div className="App">
-      
+      <div className='header'>
+        <div className='center'>
+          <div className='header__logo'>
+            <a href=''><img src='https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png' /></a>
+          </div>
+          {
+            // Se existir o user... (caso já esteja logado)
+            (user) ?
+              <div>Olá</div>
+              :
+              // Caso não esteja logado ...
+              <div className='header__loginForm'>
+                <form>
+                  <input type='text' placeholder='Login...' />
+                  <input type='password' placeholder='Senha...' />
+                  <input type='submit' name='acao' value="Logar!" />
+                </form>
+              </div>
+          }
+
+
+        </div>
+      </div>
     </div>
   );
 }
