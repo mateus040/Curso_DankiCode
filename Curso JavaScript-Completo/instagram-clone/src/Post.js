62f6e1304185819e0411a22169683dbfed7cdb3d
mateus040 <mateusgabrielmoreno264@gmail.com>
2023-06-22 18:10:36 -0300

    finalizando o projeto

--- Curso JavaScript-Completo/instagram-clone/src/Post.js
+++ Curso JavaScript-Completo/instagram-clone/src/Post.js
@@ -1,12 +1,13 @@
 import { db } from './firebase.js';
 import { useEffect, useState } from 'react';
+import firebase from 'firebase/compat/app';
 
 function Post(props) {
 
     const [comentarios, setComentarios] = useState([]);
 
     useEffect(() => {
-        db.collection('posts').doc(props.id).collection('comentarios').onSnapshot(function (snapshot) {
+        db.collection('posts').doc(props.id).collection('comentarios').orderBy('timestamp','desc').onSnapshot(function (snapshot) {
             setComentarios(snapshot.docs.map((document) => {
                 return { id: document.id, info: document.data() } // Atualizando de forma automática
             }))
@@ -20,7 +21,8 @@ function Post(props) {
 
         db.collection('posts').doc(id).collection('comentarios').add({
             nome: props.user,
-            comentario: comentarioAtual
+            comentario: comentarioAtual,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp()
         })
 
 
@@ -36,7 +38,7 @@ function Post(props) {
             <p><b>{props.info.userName}</b>: {props.info.titulo}</p>
 
             <div className='coments'>
-
+                <h2>Últimos Comentários:</h2>
                 {
                     comentarios.map(function (val) {
                         return (
