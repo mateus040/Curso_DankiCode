1c8b6ee8ff03f974d3711e02c84303ccc1c28e5e
mateus040 <mateusgabrielmoreno264@gmail.com>
2023-06-22 21:22:34 -0300

    criando rotas inicias e configurando views

--- Curso JavaScript-Completo/ProjetoNodeJS/index.js
+++ Curso JavaScript-Completo/ProjetoNodeJS/index.js
@@ -1,9 +1,23 @@
-// Criando servidor
+// Construindo servidor
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-// Definindo na porta 5000
+// Código padrão para utilizar o express com ejs
+app.engine('html', require('ejs').renderFile); // Engine para renderização do tipo html utilizando o ejs
+app.set('view engine', 'html'); // Setando a view engine para html
+app.use('/public', express.static(path.join(__dirname, 'public'))); // Mostrando o diretório estático onde fica todos os arquivos
+app.set('views', path.join(__dirname, '/views')); // Pasta onde estão as views
+
+app.get('/',(req,res)=>{
+
+    // Buscando do arquivo index dento da pasta views
+    res.render('index',{nome:'Mateus!'});
+
+});
+
+// Estabelecendo na porta 5000
 app.listen(5000,()=>{
-    console.log('server rodando');
+    console.log('server logado!');
 })
