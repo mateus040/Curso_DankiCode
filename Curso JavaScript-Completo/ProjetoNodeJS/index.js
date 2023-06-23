80b994e38c8dfc6d1123736bd910d77bb3bf6e00
mateus040 <mateusgabrielmoreno264@gmail.com>
2023-06-22 22:01:22 -0300

    criando rota deletar

--- Curso JavaScript-Completo/ProjetoNodeJS/index.js
+++ Curso JavaScript-Completo/ProjetoNodeJS/index.js
@@ -10,12 +10,24 @@ app.set('view engine', 'html'); // Setando a view engine para html
 app.use('/public', express.static(path.join(__dirname, 'public'))); // Mostrando o diretório estático onde fica todos os arquivos
 app.set('views', path.join(__dirname, '/views')); // Pasta onde estão as views
 
+var tarefas = ['Arrumar o quarto','Comprar no supermercado'];
+
+// Rota home
 app.get('/',(req,res)=>{
 
     // Buscando do arquivo index dento da pasta views
-    res.render('index',{nome:'Mateus!'});
+    res.render('index',{tarefasList:tarefas});
 
 });
+
+// Rota deletar
+app.get('/deletar/:id',(req,res)=>{
+    tarefas = tarefas.filter(function(val,index){
+            if(index != req.params.id){
+                return val;
+            }
+    })
+})
 
 // Estabelecendo na porta 5000
 app.listen(5000,()=>{
