78016737609a116824dfe93c5b44c7eeb649e4d7
mateus040 <mateusgabrielmoreno264@gmail.com>
2023-08-03 21:15:04 -0300

    hooks de estado

--- Curso_React/my-app/src/App.js
+++ Curso_React/my-app/src/App.js
@@ -1,12 +1,26 @@
-import './App.css';
-import Header from './Header'; // Importando componente Header
+import React, { useState } from "react";
 
-function App(props) {
+function App() {
+  const [count, setCount] = useState(0);
+  const [nome, setNome] = useState("Joao");
+  const [numeros, setNumeros] = useState([1,2,3,4,5,6]);
+  const [obj, setObj] = useState({nome:'Mateus', idade:'17'});
+
   return (
-    <div className="App">
-      <Header />
+    <div>
+{
+  numeros.map(function(val){
+    return(
+      <p>{val}</p>
+    )
+  })
+}
+      <p>{nome}</p>
+      <button onClick={() => setNome("Mateus")}>
+        Click me
+      </button>
     </div>
-  );
+  )
 }
 
 export default App;
